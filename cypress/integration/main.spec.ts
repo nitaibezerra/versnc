5e0c8c71046dcfbd9fe4d3091c84912d02480a47
André "decko" de Brito <brito.afa@gmail.com>
2018-01-17 17:28:57 -0200

    [UPDATE] Insere testes para os primeiros elementos da tela inicial do VerSNC.

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -1,3 +1,23 @@
 it('Retorna 200 OK ao acessar a raiz', () => {
   cy.visit('http://localhost:4200/');
 });
+
+it('Apresenta titulo da Aplicação na Tela Inicial', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('app-root h1').contains('VerSNC');
+});
+
+it('Apresenta Tabela na Pagina Inicial', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('app-root snc-table mat-table');
+});
+
+it('Apresenta cidade na tabela da Página Inicial', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('app-root snc-table mat-table mat-header-row').contains('Cidade');
+});
+
+it('Apresenta UF na tabela da Página Inicial', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('app-root snc-table mat-table mat-header-row').contains('Uf');
+});
