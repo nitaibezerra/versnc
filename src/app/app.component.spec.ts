440cee104b23f3ba54254cfd1b2b3f0ca509bdac
André "decko" de Brito <brito.afa@gmail.com>
2018-01-18 12:37:28 -0200

    [UPDATE] Atualiza o componente app-root e seus testes.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it('Titulo da Aplicação deve ser "VerSNC"', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('VerSNC');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('VerSNC');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 import { MaterialModule } from './material/material.module';
 
-import { environment } from '../environments/environment';
+import { SlcApiService } from './slc-api.service';
+
+import { AppComponent } from './app.component';
 import { SncTableComponent } from './snc-table/snc-table.component';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +24,10 @@ import { SncTableComponent } from './snc-table/snc-table.component';
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    HttpClientModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [SlcApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
