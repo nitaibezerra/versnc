4d86815996c4daef390c775c289e44677f416a3d
luizfpinheiro <luiz.pinheiro@cultura.gov.br>
2018-05-25 14:55:00 -0300

    [UPDATE] Correção e atualização de testes do cypress

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -1,6 +1,8 @@
+
 it('Retorna 200 OK ao acessar a raiz', () => {
   cy.visit('http://localhost:4200/');
 });
+
 it('Apresenta descrição Consulte seu Município ou UF', () => {
   cy.visit('http://localhost:4200/home');
   cy.get('app-root snc-busca h4').contains('Consulte o seu Município ou UF');
@@ -12,16 +14,28 @@ it('Apresenta Tabela na Pagina Inicial', () => {
   cy.get('app-root snc-table mat-card');
 });
 
-it('Apresenta município na tabela da Página Inicial', () => {
+it('Apresenta título MUNICÍPIO na tabela da Página Inicial após pesquisa', () => {
   cy.visit('http://localhost:4200/');
-  cy.get('input').type('{enter}');  
-  const button = cy.get('button mat-sort-header-container');
-  cy.get(button).contains('MUNICÍPIO');
+  cy.get('input').type('{enter}');    
+  cy.get('.mat-sort-header-button').contains('MUNICÍPIO');
 });
 
-it('Apresenta UF na tabela da Página Inicial', () => {
+it('Apresenta título SITUAÇÃO DA ADESÃO na tabela da Página Inicial após pesquisa', () => {
   cy.visit('http://localhost:4200/');
-  cy.get('app-root snc-table mat-table mat-header-row').contains('Uf');
+  cy.get('input').type('{enter}');  
+  cy.get('.mat-sort-header-button').eq(1).contains('SITUAÇÂO DA ADESÃO');
+});
+
+it('Apresenta título DATA DA ADESÃO na tabela da Página Inicial após pesquisa', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('input').type('{enter}');  
+  cy.get('.mat-sort-header-button').eq(2).contains('DATA DA ADESÃO');
+});
+
+it('Apresenta título DETALHAR na tabela da Página Inicial após pesquisa', () => {
+  cy.visit('http://localhost:4200/');
+  cy.get('input').type('{enter}');  
+  cy.get('.mat-header-cell').eq(3).contains('DETALHAR');
 });
 
 it('Apresenta 10 elementos referentes aos municipios', () => {
