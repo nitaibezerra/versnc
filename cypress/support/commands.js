acf8e7330403094957ea804e04651aa405959349
Luiz F. Pinheiro <luiz.pinheiro@cultura.gov.br>
2018-06-25 15:37:24 -0300

    Atualização da url da API usada nos testes cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,7 +32,7 @@ Cypress.Commands.add('api', () => {
   cy.server()           // enable response stubbing
   cy.route({
     method: 'GET',      // Route all GET requests
-    url: 'http://snchomolog.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
+    url: 'http://hmg.snc.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
     response: 'fixture:entidadeResponse'        // and force the response to be: []
   })
  });
@@ -41,7 +41,7 @@ Cypress.Commands.add('apiSimples', () => {
   cy.server()           // enable response stubbing
   cy.route({
     method: 'GET',      // Route all GET requests
-    url: 'http://snchomolog.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=Malhada&estado_sigla=&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
+    url: 'http://hmg.snc.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=Malhada&estado_sigla=&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
     response: 'fixture:entidade.json'        // and force the response to be: []
   })
 });
@@ -50,7 +50,7 @@ Cypress.Commands.add('api_busca_uf', () => {
   cy.server()           // enable response stubbing
   cy.route({
     method: 'GET',      // Route all GET requests
-    url: 'http://snchomolog.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=DF&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
+    url: 'http://hmg.snc.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=DF&cnpj_prefeitura=',    // that have a URL that matches '/users/*'
     response: 'fixture:entidadeResponse'        // and force the response to be: []
   })
  });
@@ -59,7 +59,7 @@ Cypress.Commands.add('api_busca_cnpj', () => {
   cy.server()           // enable response stubbing
   cy.route({
     method: 'GET',      // Route all GET requests
-    url: 'http://snchomolog.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=&cnpj_prefeitura=14.105.217/0001-70',    // that have a URL that matches '/users/*'
+    url: 'http://hmg.snc.cultura.gov.br/api/v1/sistemadeculturalocal/?limit=&offset=&nome_municipio=&estado_sigla=&cnpj_prefeitura=14.105.217/0001-70',    // that have a URL that matches '/users/*'
     response: 'fixture:entidade.json'        // and force the response to be: []
   })
 });
